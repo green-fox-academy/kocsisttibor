a98c217da66ed30c29faac68d5201436750126b0
Kocsis <kocsisttibor@gmail.com>
2017-09-11 14:08:33 +0200

    feat(week02day01): cuboid.py surface, volume calculated

--- week-02/day-01/cuboid.py
+++ week-02/day-01/cuboid.py
@@ -3,3 +3,13 @@
 # 
 # Surface Area: 600
 # Volume: 1000
+
+a_side = 10
+b_side = 20
+c_side = 30
+
+surface = 2 * (a_side * b_side) + 2 * (b_side * c_side) + 2 * (a_side * c_side)
+volume = a_side * b_side * c_side
+
+print("Surface Area: " + str(surface))
+print("Volume: " + str(volume))
