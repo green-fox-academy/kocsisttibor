fab959658de561fcb1d1853b885a5a221988e84d
Kocsis <kocsisttibor@gmail.com>
2017-09-11 16:21:25 +0200

    feat(week02day01): draw_diamond.py

--- week-02/day-01/draw_diamond.py
+++ week-02/day-01/draw_diamond.py
@@ -11,3 +11,26 @@
 #    *
 #
 # The diamond should have as many lines as the number was
+
+number = int(input("Give me a number: "))
+
+if number % 2 == 0:
+    half = number / 2
+else:
+    half = number // 2 + 1
+
+i = 1
+
+while i <= half:
+    print(" " * (number - i) + "*" * (2 * i - 1))
+    i += 1
+
+if number % 2 == 0:
+    i -= 1
+else:
+    i -= 2
+
+while i >= 1:
+    print(" " * (number - i) + "*" * (2 * i - 1))
+    i -= 1
+
