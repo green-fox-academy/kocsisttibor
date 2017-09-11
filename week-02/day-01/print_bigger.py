1481f28f8c1f0f49cdf75d4756ccf9a11b83770a
Kocsis <kocsisttibor@gmail.com>
2017-09-11 15:08:01 +0200

    feat(week02day01): print_bigger.py comparison done

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,11 @@
 # Write a program that asks for two numbers and prints the bigger one
+
+number_01 = int(input("First number: "))
+number_02 = int(input("Second number: "))
+
+if number_01 > number_02:
+    print("First number is bigger.")
+elif number_02 > number_01:
+    print("Second number is bigger.")
+else:
+    print("The two numbers are equal.")
