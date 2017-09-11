e9b36832f654065f8bb58c4d9333e5b7e32e68b7
Kocsis <kocsisttibor@gmail.com>
2017-09-11 16:09:25 +0200

    feat(week02day01): draw_pyramid.py

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -8,3 +8,10 @@
 # *******
 #
 # The pyramid should have as many lines as the number was
+
+number = int(input("Give me a number: "))
+
+i = 1
+while i <= number:
+    print(" " * (number - i) + "*" * (2 * i - 1))
+    i += 1
