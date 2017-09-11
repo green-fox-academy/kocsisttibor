237e49445588caa3ba4a29ab595c5c6fa18ca50f
Kocsis <kocsisttibor@gmail.com>
2017-09-11 17:20:57 +0200

    feat(week02day01): draw_diagonal.py

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -10,3 +10,10 @@
 # %%%%%
 #
 # The square should have as many lines as the number was
+
+number = int(input("Give me a number: "))
+
+print("%" * number)
+for i in range(2, number):
+    print("%" + " " * (i - 2) + "%" + " " * (number - i - 1) + "%")
+print("%" * number)
