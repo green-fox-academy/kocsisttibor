599b310e5da9575c28d83144b62d5f2e4e20ec63
Kocsis <kocsisttibor@gmail.com>
2017-09-11 14:15:24 +0200

    feat(week02day01): seconds_in_a_day.py remaining time calculated

--- week-02/day-01/seconds_in_a_day.py
+++ week-02/day-01/seconds_in_a_day.py
@@ -3,4 +3,10 @@ current_minutes = 34;
 current_seconds = 42;
 
 # Write a program that prints the remaining seconds (as an integer) from a
-# day if the current time is represented bt the variables
+# day if the current time is ree
+#presented bt the variablesr
+remaining_hours = 23 - current_hours
+remaining_minutes = 59 - current_minutes
+remaining_seconds = 60 - current_seconds
+
+print(str(remaining_hours) + ":" + str(remaining_minutes) + ":" + str(remaining_seconds))
