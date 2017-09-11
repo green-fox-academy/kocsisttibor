d0f30ea8643ef8d2b85509c2ef3ea18f92dcf85b
Kocsis <kocsisttibor@gmail.com>
2017-09-11 14:38:11 +0200

    feat(week02day01): average_of_input.py input set, calculation printed

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,13 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+
+number_01 = int(input("Enter first number: "))
+number_02 = int(input("Enter second number: "))
+number_03 = int(input("Enter third number: "))
+number_04 = int(input("Enter forth number: "))
+number_05 = int(input("Enter fifth number: "))
+
+summ = number_01 + number_02 + number_03 + number_04 + number_05
+
+print("Sum: " + str(summ) + ", Average: " + str(summ / 5))
