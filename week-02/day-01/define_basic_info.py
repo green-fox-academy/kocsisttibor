9e86045286d94f18d802d412fe29694289aa3446
Kocsis <kocsisttibor@gmail.com>
2017-09-11 13:44:12 +0200

    feat(week02day01): define_basic_info.py different óvaraibles set printed

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -3,3 +3,13 @@
 # Your age as an integer
 # Your height in meters as a double
 # Wether you are married or not as a boolean
+
+name = "Tibi"
+age = 36
+height = 1.8
+married = True
+
+print(name)
+print(age)
+print(height)
+print(married)
