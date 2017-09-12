fc6ea36076ad45e3bbd807c9bbe503a7f311473f
Kocsis <kocsisttibor@gmail.com>
2017-09-12 15:54:10 +0200

    feat(proba): inline for cycle creates list

---
+++ week-02/day-02/proba.py
@@ -0,0 +1,11 @@
+newlist = [x * x for x in range(10) if x % 2 == 0]
+newlist2 = [x for x in range(5, 35, 3)]
+
+print(newlist)
+print(newlist2)
+
+af = [ 4, 5, 6, 7, 8]
+
+af = [x * 2 for x in af]
+
+print(af)
