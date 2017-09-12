87547977657feac503dcb81fa83e61143cd9d298
Kocsis <kocsisttibor@gmail.com>
2017-09-12 11:27:16 +0200

    feat(print_arguments) function created

---
+++ week-02/day-02/print_arguments.py
@@ -0,0 +1,8 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer(*args):
+    print(args)
+
+printer(345, 236, "apple")
