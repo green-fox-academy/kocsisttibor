5073babebe81d2af2ff42ed62da37202ec598c1f
Kocsis <kocsisttibor@gmail.com>
2017-09-13 11:32:37 +0200

    feat(student_filter): candy counter function created

---
+++ week-02/day-03/student_filter.py
@@ -0,0 +1,20 @@
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+
+# create a function that takes a list of students and prints:
+# - Who has got more candies than 4 candies
+
+# create a function that takes a list of students and prints: 
+#  - how many candies they have on average
+
+def candies(num, students):
+    for i in range(len(students)):
+        if students[i]["candies"] > num:
+            print(students[i]["name"])
+
+candies(4, students)
+
