a7db558a667597a4f7cddb7f9a27e0e571e73bb9
Kocsis <kocsisttibor@gmail.com>
2017-09-14 10:58:31 +0200

    feat(unique): random list created

---
+++ week-02/day-04/unique.py
@@ -0,0 +1,14 @@
+# Unique - remove the duplicates
+
+# Create a function that takes a list of numbers as a parameter
+# Returns a list of numbers where every number in the list occurs only once
+# Example
+
+# input: [1, 11, 34, 11, 52, 61, 1, 34]
+# output: [1, 11, 34, 52, 61]
+
+from random import randint
+
+random_list = [randint(1, 10) for x in range(20)]
+
+print(random_list)
