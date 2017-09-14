b186f4e6013bca0767bb7bf1fabb579417b64447
Kocsis <kocsisttibor@gmail.com>
2017-09-14 15:29:21 +0200

    feat(substr): solution without .find()

--- week-02/day-04/substr.py
+++ week-02/day-04/substr.py
@@ -8,7 +8,31 @@
 # input: "this is what I'm searching in", "searching"
 # output: 17
 
-def finder (input_string, output_string):
+def built_in_finder (input_string, output_string):
     return input_string.find(output_string)
 
-print(finder("this is what I'm searching in", "searching"))
+# solution without .find()
+
+def is_in (input_string, output_string):
+    i = 0
+    while i <= len(output_string) - 1:
+        part = output_string[:i]
+        if part in input_string:
+            i += 1
+            is_in = True
+        else:
+            is_in = False
+            break
+    return is_in
+
+def position (input_string, output_string):
+    return(input_string.index(output_string))
+
+def finder (input_string, output_string):
+    if is_in(input_string, output_string) == True:  
+        return position(input_string, output_string)
+    else:
+        return -1
+
+
+print(finder("this is what I'm searching in", "I'm"))
