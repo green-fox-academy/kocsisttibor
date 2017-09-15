649b47e0dd0387c50ce642dd96749f05a06ca2c6
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-15 07:43:09 +0200

    fix(table_printer): variable names changed, code made cleaner

--- week-02/day-03/table_printer.py
+++ week-02/day-03/table_printer.py
@@ -17,7 +17,7 @@ ingredients = [
 	{ 'vodka': 1, 'needs_cooling': True },
 	{ 'coffee_liqueur': 0, 'needs_cooling': True },
 	{ 'fresh_cream': 1, 'needs_cooling': True },
-	{ 'captain_morgan_rum': 2, 'needs_cooling': True },
+	{ 'captain_morgan_spiced_rum': 2, 'needs_cooling': True },
 	{ 'mint_leaves': 0, 'needs_cooling': False },
 	{ 'sugar': 100, 'needs_cooling': False },
 	{ 'lime juice': 10, 'needs_cooling': True },
@@ -25,37 +25,46 @@ ingredients = [
 ]
 
 
-def width(in_list):             #column-width?
+def col_width(content_list):
     lengths = []
-    for i in range(len(in_list)):
-        temp_list = list(in_list[i].keys())
+    for i in range(len(content_list)):
+        temp_list = list(content_list[i].keys())
         lengths.append(len(temp_list[0]))
     return max(lengths) + 2
 
 
-def table_header(in_list):
-    print("+" + "-" * width(in_list) + "+---------------+----------+")
-    print("| Ingredient" + " " * (width(in_list) - len(" Ingredient")) + 
-    "| Needs cooling | In stock |" )
-    print("+" + "-" * width(in_list) + "+---------------+----------+")    # separator (use also for footer)
+def table_header(content_list):
+    print("+" + "-" * col_width(content_list) + "+---------------+----------+")
+    print("| Ingredient" + " " * (col_width(content_list) - len(" Ingredient"))
+     + "| Needs cooling | In stock |" )
 
 
-def table_body(in_list):
-    for i in range(len(in_list)):                   #only in_list
-        temp_list = list(in_list[i].keys())
-        if in_list[i]["needs_cooling"]:
+def separator(content_list):
+    print("+" + "-" * col_width(content_list) + "+---------------+----------+")    
+
+
+def table_body(content_list):
+    for i in range(len(content_list)):                   
+        keys = list(content_list[i].keys())
+
+        if content_list[i]["needs_cooling"]:
             cool = "Yes"
         else:
             cool = "No"
-        print("| " + temp_list[0] + " " * (width(in_list) - len(temp_list[0]) - 1) 
-        + "| " + cool + " " * (14 - len(cool)) + "| " 
-        + str(in_list[i][temp_list[0]]) + " " * 
-        (9 - len(str(in_list[i][temp_list[0]]))) + "|")             #create variables before, not in print
-    print("+" + "-" * width(in_list) + "+---------------+----------+")
+        
+        first_col = col_width(content_list) - len(keys[0]) - 1
+        second_col = 14 - len(cool)
+        stock = str(content_list[i][keys[0]])
+        third_col = 9 - len(stock)
 
-
-def table_printer(in_list):
-    table_header(in_list)
-    table_body(in_list)
+        print("| " + keys[0] + " " * first_col + "| " + cool + " " * second_col
+         + "| " + stock + " " * third_col + "|")           
+ 
+ 
+def table_printer(content_list):
+    table_header(content_list)
+    separator(content_list)
+    table_body(content_list)
+    separator(content_list)
 
 table_printer(ingredients)
