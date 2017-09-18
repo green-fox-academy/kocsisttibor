d8a93c5211cd18657c8c2758ef5fafc721bc15a7
Kocsis <kocsisttibor@gmail.com>
2017-09-18 11:55:02 +0200

    feat(print_each_line): in exception e.filename()

---
+++ week-03/day-01/print_each_line.py
@@ -0,0 +1,11 @@
+# Write a program that opens a file called "my-file.txt", then prints
+# each of lines form the file.
+# If the program is unable to read the file (for example it does not exists),
+# then it should print an error message like: "Unable to read file: my-file.txt"
+
+try:
+    with open("my-file.txt", "r") as f:
+        for line in f:
+            print(f.readline())
+except Exception as e:
+    print("cannot open: " + e.filename)
