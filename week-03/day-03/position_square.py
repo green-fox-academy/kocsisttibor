337e4cf285a0b2527eaceb892c50273de1f0271c
Kocsis <kocsisttibor@gmail.com>
2017-09-20 11:27:10 +0200

    feat(position_square): squares drawn

--- week-03/day-03/position_square.py
+++ week-03/day-03/position_square.py
@@ -10,4 +10,11 @@ canvas.pack()
 # and draws a 50x50 square from that point.
 # draw 3 squares with that function.
 
+def rectangle (x0, y0):
+    rectangle = canvas.create_rectangle(x0, y0, x0 + 50, y0 + 50)
+
+rectangle(20, 20)
+rectangle(40, 20)
+rectangle(110, 110)
+
 root.mainloop()
