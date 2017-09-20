9c24a52c1d9a037c19956920913b9fcd557aebe3
Kocsis <kocsisttibor@gmail.com>
2017-09-20 18:36:06 +0200

    feat(line_play_quarters): right-top corner partially done

--- week-03/day-03/line_play_quarters.py
+++ week-03/day-03/line_play_quarters.py
@@ -17,6 +17,14 @@ def line_from_top_to_right(x, vertical, horizontal):
 def line_from_bottom_to_left(y, vertical, horizontal):
     line = canvas.create_line(vertical, y, y, horizontal, fill = "green")
 
+
+def line_from_top_to_right_right(x, vertical, horizontal):
+    line = canvas.create_line(x, vertical, horizontal, x - horizontal / 2, fill = "purple")
+
+
+def line_from_bottom_to_left_left(y, vertical, horizontal):
+    line = canvas.create_line(vertical, y, y, horizontal, fill = "green")
+
 start = 20
 distance = 10
 vertical = 0
@@ -27,15 +35,31 @@ def seed_drawer(start, distance, vertical, horizontal):
         line_from_top_to_right(start, vertical, horizontal)
         start += distance
 
-    for j in range(int((start - vertical) / distance)):
+    for j in range(int((start - vertical) / distance) - 1):
         line_from_bottom_to_left(start - distance, vertical, horizontal)
         start -= distance
 
 seed_drawer( start, distance, vertical, horizontal)
+
 start = 170
 vertical = 150
 horizontal = 300
 seed_drawer( start, distance, vertical, horizontal)
+
+start = 170
+vertical = 0
+horizontal = 300
+
+def seed_drawer_2(start, distance, vertical, horizontal):
+    for i in range(int((horizontal - start) / distance)):
+        line_from_top_to_right_right(start, vertical, horizontal)
+        start += distance
+    start -=150
+    for j in range(int((start - vertical) / distance) - 1):
+        line_from_bottom_to_left_left(start - distance, vertical, horizontal)
+        start -= distance
+
+seed_drawer_2( start, distance, vertical, horizontal)
 
 
 root.mainloop()
