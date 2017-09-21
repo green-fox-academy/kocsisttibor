7117aa5ddd76694dd3b3c71eb7e7df08306d7f47
Kocsis <kocsisttibor@gmail.com>
2017-09-21 11:33:26 +0200

    feat(strings): x_to_y function created

---
+++ week-03/day-04/strings.py
@@ -0,0 +1,13 @@
+# Given a string, compute recursively (no loops) a new string where all the
+# lowercase 'x' chars have been changed to 'y' chars.
+
+def x_to_y(text):
+    if "x" not in text:
+        return text
+    else:
+        if text[0] == "x":
+            return "y" + x_to_y(str(text[1:]))
+        else:
+            return text[0] + x_to_y(str(text[1:]))
+
+print(x_to_y("macskx"))
