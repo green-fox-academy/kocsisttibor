4810469acec3a1b558b181d90e80c4ee1c7ae593
Kocsis <kocsisttibor@gmail.com>
2017-09-22 13:53:48 +0200

    feat(square_grid): color added

--- week-03/day-05/square_grid.py
+++ week-03/day-05/square_grid.py
@@ -7,15 +7,17 @@ root = Tk()
 canvas = Canvas(root, width='300', height='300', bg="white")
 canvas.pack()
 
-def draw_grid(x, y, size, border):
-    if size > 5:
-        canvas.create_rectangle(x, y, x + size, y + size, width=border)
 
-        draw_grid(x - size / 4, y - size / 4, size / 2, border / 2)
-        draw_grid(x + size * 3 / 4, y - size / 4, size / 2, border / 2)
-        draw_grid(x + size * 3 / 4, y + size * 3 / 4, size / 2, border / 2)
-        draw_grid(x - size / 4, y + size * 3 / 4, size / 2, border / 2)
+def draw_grid(x, y, size, border, color):
+    rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+    if size > 3:
+        canvas.create_rectangle(x, y, x + size, y + size, width=border, outline=rainbow[color])
 
-draw_grid(75, 75, 150, 15)
+        draw_grid(x - size / 4, y - size / 4, size / 2, border / 2, color + 1)
+        draw_grid(x + size * 3 / 4, y - size / 4, size / 2, border / 2, color + 1)
+        draw_grid(x + size * 3 / 4, y + size * 3 / 4, size / 2, border / 2, color + 1)
+        draw_grid(x - size / 4, y + size * 3 / 4, size / 2, border / 2, color + 1)
+
+draw_grid(75, 75, 150, 15, 0)
 
 root.mainloop()
