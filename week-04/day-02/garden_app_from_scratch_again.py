570a88a13655ba552e495f572e43d0f990c5ccea
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-26 22:48:29 +0200

    feat(garden_app_again): garden class eliminated, plant review, needs_water methods created

--- week-04/day-02/garden_app_from_scratch_again.py
+++ week-04/day-02/garden_app_from_scratch_again.py
@@ -1,18 +1,31 @@
 # https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/inheritance/garden-app/garden-app.md
 
-class Garden:
-
-    def __init__(self):
-        self.plants = []
-    
-    def add_plant(self, plant):
-        self.plants.append(plant)
-
-class Plant:
+class Plant():
     
     def __init__(self, color):
         self.color = color
         self.water_amount = 0
+        self.plants = []
+
+
+    def add_plant(self, plant):
+        self.plants.append(plant)
+
+
+    def needs_water(self):
+        for plant in self.plants:
+            if plant.water_amount < plant.water_limit:
+                return 1
+            else:
+                return 0
+
+
+    def review_of_garden(self):
+        for plant in self.plants:
+            if plant.water_amount < plant.water_limit:
+                print("The {} {} needs water. Water amount: {}".format(plant.color, plant.__class__.__name__, plant.water_amount))
+            else:
+                print("The {} {} doesn't need water. Water amount: {}".format(plant.color, plant.__class__.__name__, plant.water_amount))
 
 
 class Flower(Plant):
@@ -35,9 +48,11 @@ plant02 = Flower("blue")
 plant03 = Tree("purple")
 plant04 = Tree("orange")
 
-garden = Garden()
+plants = Plant("white")
 
-garden.add_plant(plant01)
-garden.add_plant(plant02)
-garden.add_plant(plant03)
-garden.add_plant(plant04)
+plants.add_plant(plant01)
+plants.add_plant(plant02)
+plants.add_plant(plant03)
+plants.add_plant(plant04)
+
+plants.review_of_garden()
