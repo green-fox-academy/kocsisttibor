71211d3b49763bde48c09fcce3801bf2120a451e
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-26 23:06:40 +0200

    feat(garden_app_again): watering method created

--- week-04/day-02/garden_app_from_scratch_again.py
+++ week-04/day-02/garden_app_from_scratch_again.py
@@ -13,11 +13,11 @@ class Plant():
 
 
     def needs_water(self):
+        plants_in_need = 0
         for plant in self.plants:
             if plant.water_amount < plant.water_limit:
-                return 1
-            else:
-                return 0
+                plants_in_need +=1
+        return plants_in_need
 
 
     def review_of_garden(self):
@@ -26,6 +26,17 @@ class Plant():
                 print("The {} {} needs water. Water amount: {}".format(plant.color, plant.__class__.__name__, plant.water_amount))
             else:
                 print("The {} {} doesn't need water. Water amount: {}".format(plant.color, plant.__class__.__name__, plant.water_amount))
+
+
+    def water_to_plants(self, water_in_can):
+        print("\nWatering with {}".format(water_in_can))
+        plants_in_need = self.needs_water()
+        if plants_in_need == 0:
+            print("No need of water.")
+        else:
+            for plant in self.plants:
+                if plant.water_amount < plant.water_limit:
+                    plant.water_amount += (water_in_can / plants_in_need) * plant.absorption_efficiency
 
 
 class Flower(Plant):
@@ -55,4 +66,10 @@ plants.add_plant(plant02)
 plants.add_plant(plant03)
 plants.add_plant(plant04)
 
+plants.review_of_garden()
+plants.water_to_plants(40)
+plants.review_of_garden()
+plants.water_to_plants(70)
+plants.review_of_garden()
+plants.water_to_plants(70)
 plants.review_of_garden()
