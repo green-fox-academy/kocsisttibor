7fb32aad57564af6c7b038d2c7f4eab53fa77a13
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-30 22:11:10 +0200

    feat(aircraft_carrier): carrier fight method created

--- week-04/day-02/aircraft_carrier.py
+++ week-04/day-02/aircraft_carrier.py
@@ -91,17 +91,12 @@ class Carrier(object):
             status += "\n" + str(aircraft.get_status())
         return status  
 
-f16_01 = F16()
-f16_02 = F16()
-f35_01 = F35()
-f35_02 = F35()
-carrier = Carrier(28, 5000)
 
-carrier.add_aircraft(f16_01)
-carrier.add_aircraft(f16_02)
-carrier.add_aircraft(f35_01)
-carrier.add_aircraft(f35_02)
-
-carrier.fill()
-
-print(carrier.get_status())
+    def fight(self, enemy):
+        enemy.health_point -= self.total_damage()
+        for aircraft in self.F16_store:
+            aircraft.ammo_store = 0
+        for aircraft in self.F35_store:
+            aircraft.ammo_store = 0    
+        if enemy.health_point <= 0:
+            return "It's dead Jim :("
