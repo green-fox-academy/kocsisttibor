07eb7afbe9095e9f902e12c8f98b0376cfd7f6bd
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-30 20:53:00 +0200

    feat(aircraft_carrier): aircraft get_type, get_status methods created

--- week-04/day-02/aircraft_carrier.py
+++ week-04/day-02/aircraft_carrier.py
@@ -2,6 +2,43 @@
 
 class Aircraft(object):
 
-        def __init__(self):
-            self.ammo_store = 0
+    def __init__(self):
+        self.ammo_store = 0
+
+
+    def fight(self):
+        self.damage = self.ammo_store * self.base_damage
+        self.ammo_store = 0
+        return self.damage
+
+
+    def refill(self, ammo_storage):
+        if self.max_ammo - self.ammo_store <= ammo_storage:
+            self.ammo_store = self.max_ammo
+            return ammo_storage - (self.max_ammo - self.ammo_store)
+        else:
+            self.ammo_store += ammo_storage
+            return 0
+
+
+    def get_type(self):
+        return self.__class__.__name__
+
+
+    def get_status(self):
+        return "Type {}, Ammo: {}, Base damage: {}, All damage: {}".format(self.__class__.__name__, self.ammo_store, self.base_damage, self.ammo_store * self.base_damage)
+
+class F16(Aircraft):
+
+    def __init__(self):
+        super(F16, self).__init__()
+        self.max_ammo = 8
+        self.base_damage = 30
+
+class F35(Aircraft):
+
+    def __init__(self):
+        super(F35, self).__init__()
+        self.max_ammo = 12
+        self.base_damage = 50
 
