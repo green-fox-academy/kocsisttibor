0b8c17a1bb13347f6de09aa5487d02e43e1539d9
Kocsis Tibor <kocsisttibor@gmail.com>
2017-09-30 20:28:58 +0200

    init(aircraft_carrier): Aircraft class and methods created

---
+++ week-04/day-02/aircraft_carrier.py
@@ -0,0 +1,7 @@
+# https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/inheritance/aircraft-carrier/aircraft-carrier.md
+
+class Aircraft(object):
+
+        def __init__(self):
+            self.ammo_store = 0
+
