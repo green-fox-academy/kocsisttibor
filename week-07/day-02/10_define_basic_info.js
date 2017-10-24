c9e51522d686580b41b222b2087218585f0ebc23
Kocsis <kocsisttibor@gmail.com>
2017-10-24 12:11:59 +0200

    feat(expressions_and_control_flow): variable mutations, console.log variables

---
+++ week-07/day-02/10_define_basic_info.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Tibi';
+let age = 36;
+let height = 1.8;
+let married = true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(married);
---
+++ week-07/day-02/12_cuboid.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let side = 8;
+
+console.log('Surface Area: ' + 6 * Math.pow(side, 2));
+console.log('Volume: ' + Math.pow(side, 3));
