8f436003389d007ffaf50625e1bf7bad07d01df3
Kocsis <kocsisttibor@gmail.com>
2017-10-24 14:43:06 +0200

    feat(expressions_and_control_flow): draw diagonal

---
+++ week-07/day-02/31_draw_diagonal.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var lineCount = 6;
+
+
+// Write a program that draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %   %
+// %%%%%
+//
+// The square should have as many lines as lineCount is
+
+console.log('%'.repeat(lineCount));
+for (let i = 1; i <= lineCount - 2; i += 1) {
+    console.log('%' + ' '.repeat(i - 1) + '%' + ' '.repeat(lineCount - 3 - (i - 1)) + '%');
+}
+console.log('%'.repeat(lineCount));
