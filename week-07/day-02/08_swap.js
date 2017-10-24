43fdcaaf9a5ecb450596e7dbb7a0152d35e55647
Kocsis <kocsisttibor@gmail.com>
2017-10-24 11:54:40 +0200

    feat(expressions_and_control_flow): console.log expressions with numbers

---
+++ week-07/day-02/08_swap.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+// Swap the values of these variables
+var a = 123;
+var b = 526;
+
+console.log(a);
+console.log(b);
+
+let c = a
+ a = b
+ b = c
+
+ console.log(a);
+ console.log(b);
