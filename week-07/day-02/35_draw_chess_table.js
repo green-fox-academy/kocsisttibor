b3cd006996063bf651a2018fdd77e23322f46e37
Kocsis <kocsisttibor@gmail.com>
2017-10-24 14:48:04 +0200

    feat(expressions_and_control_flow): draw chess table

---
+++ week-07/day-02/35_draw_chess_table.js
@@ -0,0 +1,16 @@
+
+// Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % % 
+//  % % % %
+
+for (let i = 1; i <= 4; i += 1) {
+    console.log('% '.repeat(4))
+    console.log(' %'.repeat(4))
+}
