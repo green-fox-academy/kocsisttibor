e55c41703579e9ca5efd82fd9ce0278406722714
Kocsis <kocsisttibor@gmail.com>
2017-10-24 13:55:47 +0200

    feat(expressions_and_control_flow): multiplication table and FizzBuzz exercises

---
+++ week-07/day-02/25_multiplication_table.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var number = 15;
+
+// Create a program that
+// prints the multiplication table with number
+
+for (let i = 1; i <= 10; i += 1) {
+    console.log(i + ' * ' + number + ' = ' + i * number)
+}
