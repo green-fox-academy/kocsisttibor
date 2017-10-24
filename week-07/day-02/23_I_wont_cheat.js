d928b138531ec5bee582c281b5b52ae8e3c7d171
Kocsis <kocsisttibor@gmail.com>
2017-10-24 13:46:38 +0200

    feat(expressions_and_control_flow): I won't cheat and print even exercises

---
+++ week-07/day-02/23_I_wont_cheat.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (let i = 1; i <= 100; i += 1) {
+    console.log('I won\'t cheat on the exam!')
+}
---
+++ week-07/day-02/24_print_even.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+for (let i = 0; i <= 500; i += 1) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
