87a90806d59de1b881b0a4ddca8c6d19c8104ff6
Kocsis <kocsisttibor@gmail.com>
2017-10-25 15:01:01 +0200

    feat(functions_arrays_data_structures): reverse a sentence

---
+++ week-07/day-03/string_02.js
@@ -0,0 +1,8 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+var original = reversed.split('').reverse().join('');
+
+console.log(original);
