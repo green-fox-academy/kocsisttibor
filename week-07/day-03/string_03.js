d3c515a071d5b531946b89e13735cce7cf2367b3
Kocsis <kocsisttibor@gmail.com>
2017-10-25 15:18:01 +0200

    feat(functions_arrays_data_structures): append letter to every element of an array with forEach

---
+++ week-07/day-03/string_03.js
@@ -0,0 +1,11 @@
+'use strict';
+// Add "a" to every string in far
+
+var far = ["kuty", "macsk", "kacs", "rók", "halacsk"];
+
+far.forEach(function(animal, i) {
+    far[i] += 'a';
+})
+
+console.log(far);
+
