47ba84a65a25642cb7a019c19d47d2be03960b93
Kocsis <kocsisttibor@gmail.com>
2017-10-29 12:23:40 +0100

    feat(image_gallery): big picture changes by arrow click; arrow changes on hover, click; arrow becomes inactive at 0. or last item on picture list

--- week-07/day-05/gallery.js
+++ week-07/day-05/gallery.js
@@ -16,6 +16,14 @@ thumbnails.forEach(function(e, i) {
 })
 
 let image = document.querySelector('div.image');
+let image_index = 0;
+
+function image_setter() {
+    image.style.backgroundImage = 'url(' + images[image_index].url + ')';
+}
+
+image_setter();
+
 let description = document.querySelector('div.description');
 
 image.addEventListener('mouseenter', function () {
@@ -25,3 +33,45 @@ image.addEventListener('mouseenter', function () {
 image.addEventListener('mouseleave', function () {
     description.classList.add('hidden');
 });
+
+thumbnails.forEach(function(e, i) {
+    e.addEventListener('mouseenter', function() {
+        e.classList.add('before_select')
+    });
+    e.addEventListener('mouseleave', function() {
+        e.classList.remove('before_select')
+    });  
+});
+
+let side = document.querySelectorAll('.side');
+
+side.forEach(function(e){
+    e.addEventListener('mouseenter', function () {
+        e.classList.add('button_hover');
+    });
+    
+    e.addEventListener('mouseleave', function () {
+        e.classList.remove('button_hover');
+    });
+})
+
+let left_side = document.querySelector('.left.side')
+let right_side = document.querySelector('.right.side')
+
+left_side.addEventListener('click', function() {
+    image_index -= 1;
+    image_setter();
+    right_side.classList.remove('inactive')
+    if (image_index === 0) {
+        left_side.classList.add('inactive')
+    }
+})
+
+right_side.addEventListener('click', function() {
+    image_index += 1;
+    image_setter();
+    left_side.classList.remove('inactive')
+    if (image_index === images.length) {
+        right_side.classList.add('inactive')
+    }
+})
