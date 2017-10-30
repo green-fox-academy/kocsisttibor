731305bb39463aeddf2aeed65e9601100e7204cb
Kocsis <kocsisttibor@gmail.com>
2017-10-30 16:43:48 +0100

    feat(animal_farm): forEach with bind(this)

--- week-08/day-01/animal_farm.js
+++ week-08/day-01/animal_farm.js
@@ -19,3 +19,39 @@ class Animal {
     }
 }
 
+let animal = new Animal();
+
+class Farm {
+
+    constructor(number) {
+        this.listOfAnimals = [];
+        this.maxOfAnimals = number
+        for (let i = 1; i <= number; i += 1) {
+            this.listOfAnimals.push(animal);
+        }
+    }
+
+    breed() {
+        if (this.listOfAnimals.length < this.maxOfAnimals) {
+            this.listOfAnimals.push(animal);
+        }
+    }
+
+    slaughter() {
+        this.listOfAnimals.forEach(function(e, i) {
+            let fattest_index = 0;
+            let hunger = this.listOfAnimals[0].hunger;
+            if (e.hunger < hunger) {
+                hunger = e.hunger;
+                fattest_index = i;
+            }
+        }.bind(this))
+        this.listOfAnimals.splice(i, 1);
+    }
+}
+
+let farm = new Farm(5);
+farm.listOfAnimals[0].eat()
+// console.log(farm);
+farm.slaughter();
+// console.log(farm);
