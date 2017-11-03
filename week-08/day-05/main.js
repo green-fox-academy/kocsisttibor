5adbd517843d88d9e3939c1dc84aea2fa74fd3fe
Kocsis <kocsisttibor@gmail.com>
2017-11-03 15:33:13 +0100

    feat(reddit): post section's structure creater added using template

--- week-08/day-05/main.js
+++ week-08/day-05/main.js
@@ -1,14 +1,36 @@
 'use strict';
-function getPosts() {
+function getPosts(callback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://secure-reddit.herokuapp.com/posts');
-    xhr.onreadystate = function() {
-        console.log(xhr.result)
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
-            console.log(JSON.parse(xhr.responseText));
+            callback(JSON.parse(xhr.responseText));
         }
     }
     xhr.send();
 }
 
-window.onload = getPosts();
+function createSection (data) {
+    let structure = `
+        <div class = "vote_block">
+            <div></div>
+            <div>2.342</div>
+            <div></div>
+        </div>
+        <article>
+            <div>post</div>
+            <div>owner</div>
+            <ul>
+                <li>comments</li>
+                <li>modify</li>
+                <li>remove</li>
+            </ul>
+        </article>
+    `
+    let container = document.querySelector('container.left');
+    let section = document.createElement('section');
+    section.innerHTML = structure;
+    container.appendChild(section)
+}
+createSection()
+// window.onload = getPosts(createPosts);
