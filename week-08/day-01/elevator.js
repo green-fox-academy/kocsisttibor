b657341cbd9e933747501113f626fab82d226df5
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-05 15:56:21 +0100

    feat(elevator): model and view instatiated in controller's constructor

--- week-08/day-01/elevator.js
+++ week-08/day-01/elevator.js
@@ -1,5 +1,9 @@
 class elevatorView {
 
+    constructor(maxLevels) {
+        this.drawLevels(maxLevels);
+    }
+
     drawLevels(maxLevels) {
         for (let i = maxLevels; i > 0; i -= 1) {
             let div = document.createElement('div');
@@ -10,28 +14,45 @@ class elevatorView {
     }
 }
 
-elevator = new elevatorView;
-elevator.drawLevels(10);
-
 class elevatorModel {
-
+    
     constructor(maxPeople, maxLevels) {
         this.maxPeople = maxPeople;
         this.maxLevels = maxLevels;
         this.actualLevel = 1;
         this.actualPeople = 0;
     }
-
+    
     addPeople() {
         if (this.actualPeople < this.maxPeople) {
             this.actualPeople += 1;
         }
     }
-
+    
     removePeople() {
         if (this.actualPeople > 0) {
             this.actualPeople -= 1;
         }
     }
+
+    levelUp() {
+        if (this.actualLevel < this.maxLevels) {
+            this.actualLevel += 1;
+        }
+    }
+
+    levelDown() {
+        if (this.actualLevel > 1) {
+            this.actualLevel -= 1;
+        }
+    }
 }
-class elevatorController {}
+class elevatorController {
+
+    constructor(maxLevels, maxPeople) {
+        this.view = new elevatorView(maxLevels);
+        this.model = new elevatorModel(maxLevels, maxPeople);
+    }
+}
+
+elevator = new elevatorController(10, 5);
