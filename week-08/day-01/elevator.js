54cc59f24ea6edc1dc50893fb8b638855f60af6e
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-05 15:42:10 +0100

    feat(elevator): drawLevels method

--- week-08/day-01/elevator.js
+++ week-08/day-01/elevator.js
@@ -1,4 +1,18 @@
-class elevatorView {}
+class elevatorView {
+
+    drawLevels(maxLevels) {
+        for (let i = maxLevels; i > 0; i -= 1) {
+            let div = document.createElement('div');
+            div.classList.add('level_' + i)
+            let floors = document.querySelector('div.floors');
+            floors.appendChild(div);
+        }
+    }
+}
+
+elevator = new elevatorView;
+elevator.drawLevels(10);
+
 class elevatorModel {
 
     constructor(maxPeople, maxLevels) {
