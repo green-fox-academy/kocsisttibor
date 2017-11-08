38a47f665ea19178179ad40970edb0b00b31600f
Kocsis <kocsisttibor@gmail.com>
2017-11-08 13:52:44 +0100

    feat(frontend): doubling; query.input used to create and response in json format)

--- week-09/day-03/frontend/index.js
+++ week-09/day-03/frontend/index.js
@@ -1,9 +1,18 @@
 let express = require('express');
 let app = express();
-app.use('./assets', express.static('./assets'));
+app.use('/assets', express.static('./assets'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(3000);
+app.get('/doubling', function(req, res) {
+    if (req.query.input !== undefined) {
+        res.json({'received': req.query.input * 1, 'result': req.query.input * 2});
+    } else {
+        res.json({'error': 'Please provide an input!'});
+    }
+});
+
+
+app.listen(8080);
