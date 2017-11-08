1273bd6e737e1632d2f7534da9c90e8472830f73
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-08 22:55:23 +0100

    feat(reverser_of_the_sith): sentences grabbed from post request body, words order changed and mixed with random sith words

---
+++ week-09/day-03/reverser_of_the_sith/index.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+let urlencodedParser = express.urlencoded({extended: false});
+let port = 8080;
+app.use(express.json());
+
+function changeWordOrder (words) {
+    let yoda = []
+    for (let i = 0; i + 2 <= words.length; i += 2) {
+        yoda.push(words[i + 1]);
+        yoda.push(words[i]);
+    }
+    return yoda;
+}
+
+function convert (sentences) {
+    let result = [];
+    let randoms = ['Uhm', 'Urggh', 'Hmm...', 'Hm..hm', 'Grrrh', 'Argggh', 'Groh']
+    sentences.forEach(function(sentence) {
+        let words = sentence.split(' ');
+        words[0] = words[0].toLowerCase();
+        let yoda = changeWordOrder(words);
+        if (words.length > yoda.length) {
+            yoda.push(words[words.length - 1]);
+        }
+        yoda[0] = yoda[0].charAt(0).toUpperCase() + yoda[0].slice(1)
+        yoda[yoda.length - 1] += '.'; 
+        result.push(yoda.join(' '))
+        result.push(randoms[Math.round(Math.random() * randoms.length)])
+    })
+    return result.join(' ');
+}
+
+app.post('/', urlencodedParser, function(req, res){
+    let words = req.body.text.split('.');
+    res.json({'sith-text': convert(words)})
+});
+
+
+app.listen(port);
