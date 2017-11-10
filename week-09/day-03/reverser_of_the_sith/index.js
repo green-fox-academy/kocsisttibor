f67e25ac3c94264ff662a3b31e35874344aed480
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-10 01:09:58 +0100

    feat(reverser_of_the_sith): preparations for demo

--- week-09/day-03/reverser_of_the_sith/index.js
+++ week-09/day-03/reverser_of_the_sith/index.js
@@ -7,52 +7,42 @@ let port = 8080;
 app.use(express.json());
 app.use(cors());
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
-
 function changeWordOrder (words) {
-    let yoda = []
+    let yodaOrder = []
     for (let i = 0; i + 2 <= words.length; i += 2) {
-        yoda.push(words[i + 1]);
-        yoda.push(words[i]);
+        yodaOrder.push(words[i + 1]);
+        yodaOrder.push(words[i]);
     }
-    return yoda;
+    return yodaOrder;
 }
 
-function convert (sentences) {
-    let result = [];
+function convertToYodaish (sentences) {
+    let newSentences = [];
     let randoms = ['Uhm', 'Urggh', 'Hmm...', 'Hm..hm', 'Grrrh', 'Argggh', 'Groh']
     sentences.forEach(function(sentence) {
         let words = sentence.split(' ');
         words[0] = words[0].toLowerCase();
-        let yoda = changeWordOrder(words);
-        if (words.length > yoda.length) {
-            yoda.push(words[words.length - 1]);
+
+        let yodaOrder = changeWordOrder(words);
+        
+        if (words.length > yodaOrder.length) {
+            yodaOrder.push(words[words.length - 1]);
         }
-        yoda[0] = yoda[0].charAt(0).toUpperCase() + yoda[0].slice(1)
-        yoda[yoda.length - 1] += '.'; 
-        result.push(yoda.join(' '))
-        result.push(randoms[Math.round(Math.random() * randoms.length)])
+        yodaOrder[0] = yodaOrder[0].charAt(0).toUpperCase() + yodaOrder[0].slice(1)
+        yodaOrder[yodaOrder.length - 1] += '.'; 
+        newSentences.push(yodaOrder.join(' '))
+        newSentences.push(randoms[Math.round(Math.random() * randoms.length)])
     })
-    return result.join(' ');
+    return newSentences.join(' ');
 }
 
-// app.post('/', urlencodedParser, function(req, res){
-//     if (req.body.text === '' || req.body.text === undefined){
-//         res.json({'error': 'Feed me some text you have to, padawan young you are. Hmmm.'})
-//     } else {
-//         let words = req.body.text.split('.');
-//         res.json({'sith-text': convert(words)})
-//     }
-// });
-
-app.post('/', function(req, res) {
-    // res.setRequestHeader("Access-Control-Allow-Origin", "*");
-    res.send('Landed');
-})
-
+app.post('/', urlencodedParser, function(req, res){
+    if (req.body.text === '' || req.body.text === undefined){
+        res.json({'error': 'Feed me some text you have to, padawan young you are. Hmmm.'})
+    } else {
+        let words = req.body.text.split('.');
+        res.json({'sith-text': convertToYodaish(words)})
+    }
+});
 
 app.listen(port);
--- week-09/day-03/reverser_of_the_sith/padawan.js
+++ week-09/day-03/reverser_of_the_sith/padawan.js
@@ -4,8 +4,15 @@ function getWisdoms() {
     console.log('started');
     let xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8080');
-    xhr.send();
-    console.log(xhr.responseText);
+    let input = document.querySelector('input').value;
+    let data = {"text": input};
+    console.log(data)
+    xhr.send(data);
+    // document.querySelector('div.answer').innerHTML = xhr.responseText;
+    let answer = JSON.parse(xhr.responseText)
+    console.log(answer);
 }
 
-document.querySelector('button').addEventListener('click', getWisdoms)
+document.querySelector('button').addEventListener('click', function() {
+    document.querySelector('div.grr').style.display = 'block';
+})
