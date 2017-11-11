4eaca66278b3e93cb5b877f19db5f38b1fd69a60
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-11 20:45:22 +0100

    feat(reddit backend): post /posts endpoint created, frontend is linked to backend server

--- week-08/day-05/add_post.js
+++ week-08/day-05/add_post.js
@@ -5,24 +5,25 @@ const postData = {
     "url": ""
 }
 
-function post(data) {
+function post(data, callback) {
     let xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://secure-reddit.herokuapp.com/simple/posts');
+    xhr.open('POST', 'http://localhost:8080/posts');
     xhr.setRequestHeader('Content-type', 'application/json');
     xhr.onload = function() {
-        console.log(this.responseText);
+        console.log('server\'s response: ' + this.response);
+        callback();
     }
     xhr.send(JSON.stringify(data));   //string need to be sent to server, so first the objects shoulb be converted into string
 }
+
+const backToMainPage = () => window.location.href = 'main.html'     //redirects back to main page
 
 let postButton = document.querySelector('button');
 postButton.addEventListener('click', function() {
     postData.title = document.querySelector('input.title').value;
     postData.url = document.querySelector('input.url').value;
-    console.log(postData);
     if ( postData.title !== '') {
-        post(postData);
-        window.location.href = 'main.html'     //redirects back to main page
+        post(postData, backToMainPage);
     } else {
         alert('Please don\'t let the title field empty');
     }
--- week-09/day-05/server.js
+++ week-09/day-05/server.js
@@ -15,11 +15,8 @@ const connection = mysql.createConnection({
     database: 'reddit'
 });
 
+app.use(express.json());
 app.use(cors());
-
-app.get('/hello', (req, res) => 
-    res.send('hello world')
-);
 
 app.get('/posts', (req, res) =>
     connection.query('SELECT * FROM posts', (err, rows) => {
@@ -42,5 +39,39 @@ app.get('/posts', (req, res) =>
         }
     })
 )
+
+const insertPostToDatabase = (post, callback) => {
+    post.timestamp = Date.now();
+    connection.query('INSERT INTO posts SET ?', post, (err, result) => {
+        if (err) {
+            console.error(err);
+            return 
+        } else {
+            console.error(result);
+            callback(result.insertId)
+        }
+    })
+}
+
+const getAddedPostTitle = (result_id, callback) => {
+    connection.query('SELECT * FROM posts WHERE post_id =' + connection.escape(result_id),  (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            console.error('result of getAddedPostTitle' + result);
+            callback(result);
+        }
+    });
+}
+
+app.post('/posts', (req, res) => {
+    let post = req.body;
+    insertPostToDatabase(post, (result_id) => {         //callback function is used to have some proof that the data is inserter into database
+        getAddedPostTitle(result_id, (result) => {      //proof is got by a new query in database, with the uniq id of the newly added record
+            res.json(result);                           //result of the query about the newly added record is sent back to the frontend as response/feedback
+        });
+    });
+});
 
 app.listen(port, error => error ? console.log(error): console.log('Server running, at ' + port));
