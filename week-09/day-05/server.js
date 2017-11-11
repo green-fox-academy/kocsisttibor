e538b0cdacf110a0e861979097fb04955cb1690e
Kocsis Tibor <kocsisttibor@gmail.com>
2017-11-11 14:01:56 +0100

    feat(reddit backend): sql database created, connection created with backend server

--- week-09/day-05/server.js
+++ week-09/day-05/server.js
@@ -4,9 +4,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'alma',
+    database: 'reddit'
+});
+
 app.get('/hello', (req, res) => 
     res.send('hello world')
 );
-
 
 app.listen(port, error => error ? console.log(error): console.log('Server running, at ' + port));
