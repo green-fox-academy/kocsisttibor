c5ca763af799341e06114327c16c7b87b9f87d5b
Kocsis <kocsisttibor@gmail.com>
2017-11-30 23:05:46 +0100

    feat(angular-todo-app): service created to get todos from mock backend

--- project-phase/angular-todo-app/src/app/app.module.ts
+++ project-phase/angular-todo-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TodosService } from './todos.service';
 
 
 @NgModule({
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    TodosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
