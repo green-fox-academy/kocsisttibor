c860abf14cf770f91dcfaf46a3c50d06c8f86ca5
Kocsis Tibor <kocsisttibor@gmail.com>
2017-12-04 21:44:27 +0100

    refactor(reversed_order.py): it's shorter now

--- week-03/day-01/reversed_order.py
+++ week-03/day-01/reversed_order.py
@@ -3,11 +3,6 @@
 def decrypt(file_name):
     with open(file_name) as f:
         lines = list(f.readlines())
-    line = len(lines) - 1
-    decrypted = []
-    while line >= 0:
-        decrypted.append(lines[line])
-        line -= 1
-    return("".join(decrypted))
+    return("".join(lines[::-1]))
 
 print(decrypt("reversed_order.txt"))
